a9ca23bf08946edb7cc5e971c4e5b30b4cd8e785
Kestin Goforth <kgoforth1503@gmail.com>
2017-02-24 20:56:29 -0500

    open the right window

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import sys
 from PyQt5 import QtWidgets, QtGui
-from ui import ScannerWindow
+from ui import MainWindow
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     app.setWindowIcon(QtGui.QIcon('img/icon.png'))
-    window = ScannerWindow()
+    window = MainWindow()
     sys.exit(app.exec_())
