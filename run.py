936edb6ab7d1099911408e7783703fa1d68b9bc9
Kestin Goforth <kgoforth1503@gmail.com>
2018-01-05 11:17:24 -0500

    Add Fusion theme and rearrange scan window

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 import sys
+
 from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtWidgets import QStyleFactory
+
 from views import MainWindow
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     app.setWindowIcon(QtGui.QIcon('img/icon.png'))
+
+    app.setStyle(QStyleFactory.create('fusion'))
+
     window = MainWindow()
     sys.exit(app.exec_())
