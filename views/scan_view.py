81e4096e8043d4d3026bde910f535c69f2b24029
Kestin Goforth <kgoforth1503@gmail.com>
2018-01-05 11:00:30 -0500

    draw boxes on points for four corners fix

--- views/scan_view.py
+++ views/scan_view.py
@@ -63,6 +63,7 @@ class ScanView(QMainWindow):
         self.backup_img = np.zeros((1, 1, 3), np.uint8)
         self.img = np.zeros((1, 1, 3), np.uint8)
         self.raw_img = np.zeros((1, 1, 3), np.uint8)
+        self.current_img = np.zeros((1, 1, 3), np.uint8)
         self.selected_img = "img"
         self.filename = ""
         self.data_types = {}
@@ -86,8 +87,11 @@ class ScanView(QMainWindow):
             img_h, img_w = self.raw_img.shape[:-1]
             w_scale = img_w / self.scan_preview.size().width()
             h_scale = img_h / self.scan_preview.size().height()
-            point = tuple(map(int, (event.x() * w_scale, event.y() * h_scale)))
-            self.corners.append(point)
+            x = int(event.x() * w_scale)
+            y = int(event.y() * h_scale)
+            self.corners.append((x, y))
+            cv2.rectangle(self.current_img, (x - 10, y - 10), (x+10, y+10), (0, 255, 0), thickness=10)
+            self.set_img(self.current_img)
             if len(self.corners) == 4:
                 selected_points = sorted(self.corners, key=lambda l: sum(l))
                 new_points = ((200, 200), (img_w - 200, 200), (200, img_h - 200), (img_w - 200, img_h - 200))
@@ -101,9 +105,11 @@ class ScanView(QMainWindow):
         if self.selected_img == 'img':
             self.selected_img = 'raw'
             self.set_img(self.raw_img)
+            self.current_img = np.copy(self.raw_img)
         else:
             self.selected_img = 'img'
             self.set_img(self.img)
+            self.current_img = np.copy(self.img)
 
     def handle_four_corners_button(self):
         if self.click_mode == "four_corners":
@@ -190,6 +196,7 @@ class ScanView(QMainWindow):
         self.get_new_scan()
 
     def set_img(self, cv_img):
+        self.current_img = np.copy(cv_img)
         height, width, channels = cv_img.shape
         cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
         q_image = QImage(cv_img.data, width, height, width * 3, QImage.Format_RGB888)
